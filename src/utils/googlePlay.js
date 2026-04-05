e6bfdd8c9382eb94653f7573ac6841f53bc86f4c
ailanaz <117781743+ailanaz@users.noreply.github.com>
2026-04-05 09:44:25 -0500

    v

--- src/utils/googlePlay.js
+++ src/utils/googlePlay.js
@@ -1,7 +1,7 @@
 const configuredPlayPackage = import.meta.env.VITE_PLAY_APP_PACKAGE || ''
 const configuredPlayStoreUrl = import.meta.env.VITE_PLAY_STORE_URL || ''
 
-export const PLAY_APP_PACKAGE = configuredPlayPackage || 'com.fortressforesight.app'
+export const PLAY_APP_PACKAGE = configuredPlayPackage || 'app.fortressforesight'
 export const PLAY_STORE_URL = configuredPlayStoreUrl
   || (configuredPlayPackage ? `https://play.google.com/store/apps/details?id=${configuredPlayPackage}` : '')
 
